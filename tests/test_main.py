10370861a2163a0ce9004d54aa7b96a3c8142f66
louis2607 <36734223+louis2607@users.noreply.github.com>
2022-12-22 16:01:36 +0100

    feat(sub): added substraction (#2)
    * feat(sub): added substraction
    
    * feat(sub): added substraction
    
    Co-authored-by: lcottereau <lcottereau@reseau.free.fr>

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,9 @@
-from test_python_package_with_copier.main import add
+from test_python_package_with_copier.main import add, sub
 
 
 def test_add():
     assert add(1, 1) == 2
+
+
+def test_sub():
+    assert sub(3, 1) == 2
